548164f32da2d9f80e2ba7695ce185523ac6f2e1
labing506 <labing506@labing50619.udistrital.edu.co>
2017-03-01 22:50:13 -0500

    iteracion 1

---
+++ DadosApuesta/src/dadosapuesta/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dadosapuesta;
+
+/**
+ *
+ * @author labing506
+ */
+public class Principal {
+    
+     public static void main(String[] args) {
+        
+       TableroJuegos miTablero=new TableroJuegos();
+       miTablero.setVisible(true);
+             
+               
+        
+    }
+    
+}
